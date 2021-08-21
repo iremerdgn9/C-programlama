cca11b2d222dd96e69741a1c75caac33f853c96b
İrem Erdoğan <47084468+iremerdgn9@users.noreply.github.com>
2021-08-21 12:11:51 +0300

    Rename bankamatikhesabı.c to Girilen para miktarını banknot olarak ayırma ve adetlerini bulma.c
    Girilen para miktarını banknot olarak ayırma ve adetlerini bulma

--- Girilen para miktarını banknot olarak ayırma ve adetlerini bulma.c
+++ Girilen para miktarını banknot olarak ayırma ve adetlerini bulma.c
@@ -5,13 +5,13 @@ int main()
     int paraMiktari;
     int yuz,elli,yirmi,on,kalan;
     
-    printf("para miktarını giriniz: "); //para miktarını 10'un katları şeklinde klavyeden giriyoruz.
+    printf("para miktarÄ±nÄ± giriniz: "); //para miktarÄ±nÄ± 10'un katlarÄ± ÅŸeklinde klavyeden giriyoruz.
     scanf("%d",&paraMiktari);
     
     if(paraMiktari%10==0)
     {
         
-        yuz=paraMiktari/100; //tek tek miktarına göre ayrım yapıyoruz.
+        yuz=paraMiktari/100; //tek tek miktarÄ±na gÃ¶re ayrÄ±m yapÄ±yoruz.
         kalan=paraMiktari%100;
         elli=kalan/50;
         kalan=kalan%50;
@@ -20,7 +20,7 @@ int main()
         on=kalan/10;
         kalan=kalan%10;
         
-        printf("bankamatik hesabı:\n"); //ekrana yazdırıyoruz.
+        printf("bankamatik hesabÄ±:\n"); //ekrana yazdÄ±rÄ±yoruz.
         printf("%d adet 100 tl ,%d adet 50 tl\n",yuz,elli);
         printf("%d adet 20 tl  ,%d adet 10 tl\n",yirmi,on);
     }
